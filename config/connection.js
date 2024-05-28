be984aab24642cb2c84e83f4aca863766c89ae38
shwetakadam5 <159270090+shwetakadam5@users.noreply.github.com>
2024-05-28 21:06:25 +1000

    Changes related to connection details (Added commented part to uncomment while connected to render database)

--- config/connection.js
+++ config/connection.js
@@ -13,8 +13,14 @@ if (process.env.DB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: process.env.DB_HOST || 'localhost',
       dialect: 'postgres',
+      // dialectOptions: {
+      //   ssl: {
+      //     require: true,
+      //     rejectUnauthorized: false,
+      //   },
+      // },
     },
   );
 }
